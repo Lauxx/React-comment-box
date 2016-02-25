bdff30868cc9e05ca3b39843d24d0f785ea6d36a
Lauxx <lm.nichols@hotmail.com>
2016-02-25 14:07:37 -0700

    this is our react comment box facebook tutorial

---
+++ notes.js
@@ -0,0 +1,25 @@
+//my notes for the nonsense!
+//have package json - need to npm install
+//start server! nodemon server.js in correct directory
+//we changed script tag and created new react-comments.js file
+//did a console.log smoke test in react-comments.js; should show in browser console
+//next we added CommentBox to react-comments.js - following same format as before
+//ReactDOM.render() instantiates the root component, starts the framework, and injects the markup into a raw DOM element, provided as the second argument.
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
